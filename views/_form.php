1a2335243953b0d4aebd8e3db4f5aed26cbca9fa
Вешкуров Артем <veshkurov.a.g@saturn-r.ru>
2016-09-12 11:12:32 +0500

    12.09.2016 // tree manager

--- views/_form.php
+++ views/_form.php
@@ -196,22 +196,6 @@ echo $renderContent(Module::VIEW_PART_1);
         <?= $form->field($node, $nameAttribute)->textInput($inputOpts) ?>
             </div>
         </div>
-        <div class="row">
-                <div class="col-sm-12">
-                    <div class="btn-group btn-group-justified" role="group">
-                        <div class="btn-group" role="group">
-                                    <button type="button" class="btn btn-default waves-effect">Добавить руководителя</button>
-                        </div>
-                        <div class="btn-group" role="group">
-                                    <button type="button" class="btn btn-default waves-effect">Добавить наблюдателя</button>
-                        </div>
-                        <div class="btn-group" role="group">
-                                    <button type="button" class="btn btn-default waves-effect">Добавить исполнителя</button>
-                        </div>
-                    </div>
-                </div>
-
-        </div>
     <?php else: ?>
         <?= $keyField ?>
         <?= $form->field($node, $nameAttribute)->textInput($inputOpts) ?>
@@ -242,16 +226,6 @@ echo $renderContent(Module::VIEW_PART_1);
             ])
             ?>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class="btn-group btn-group" role="group">
-                        <button type="button" class="btn btn-default">Добавить руководителя</button>
-                        <button type="button" class="btn btn-default">Добавить наблюдателя</button>
-                        <button type="button" class="btn btn-default">Добавить исполнителя</button>
-                    </div>
-                </div>
-
             </div>
 
 
